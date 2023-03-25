6b2a71b7911bda1383de903115158ba656ec02c7
Paul Harrison <a02382371@aggies.usu.edu>
2023-03-25 14:43:41 -0600

    hide toolbar

--- src/pages/aggiecanvas.js
+++ src/pages/aggiecanvas.js
@@ -64,7 +64,7 @@ export default function Canvas() {
         palleteOpen={palleteOpen}
         setPalleteOpen={setPalleteOpen}
       />
-      <Toolbar />
+      {/* <Toolbar /> */}
       <ColorPallete
         grid={grid}
         focus={focus}
