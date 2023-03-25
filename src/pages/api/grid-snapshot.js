f17fe0125acd5d9f115b8510f3f105a31487da9b
Paul Harrison <a02382371@aggies.usu.edu>
2023-03-24 19:37:04 -0600

    api format

--- src/pages/api/grid-snapshot.js
+++ src/pages/api/grid-snapshot.js
@@ -2,9 +2,7 @@ import { prisma } from "../../../utils/db-client";
 
 export default async (req, res) => {
   const grid = await prisma.gridSnapshot.findFirst({
-    orderBy: {
-      lastUpdate: "desc",
-    },
+    orderBy: { lastUpdate: "desc" },
   });
 
   res.status(200).json({ grid });
