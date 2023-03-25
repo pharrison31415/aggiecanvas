3179cf0652cb8972af362047295858859edd0670
Paul Harrison <a02382371@aggies.usu.edu>
2023-03-24 22:17:54 -0600

    client request snapshot and updates, process

--- src/pages/aggiecanvas.js
+++ src/pages/aggiecanvas.js
@@ -1,10 +1,27 @@
-import React, { Component } from "react";
-import { CanvasComponent } from "@/components/canvasComponent";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-export default function Home() {
-  return (
-    <>
-      <CanvasComponent />
-    </>
-  );
+export default function Canvas() {
+  const [mainGrid, setMainGrid] = useState([]);
+
+  useEffect(() => {
+    axios.get("/api/grid-snapshot").then((gridResponse) => {
+      let snapshot = gridResponse.data.snapshot;
+      let editingGrid = snapshot.grid;
+
+      axios
+        .get("/api/latest-updates", {
+          params: { "last-update": snapshot.lastUpdate },
+        })
+        .then((updatesResponse) => {
+          let updates = updatesResponse.data.updates;
+          for (let u of updates) {
+            editingGrid[u.row][u.column] = u.color;
+          }
+          setMainGrid(editingGrid);
+        });
+    });
+  }, []);
+
+  return <div>HELLO</div>;
 }
--- src/pages/api/grid-snapshot.js
+++ src/pages/api/grid-snapshot.js
@@ -1,9 +1,9 @@
 import { prisma } from "../../../utils/db-client";
 
 export default async (req, res) => {
-  const grid = await prisma.gridSnapshot.findFirst({
+  const snapshot = await prisma.gridSnapshot.findFirst({
     orderBy: { lastUpdate: "desc" },
   });
 
-  res.status(200).json({ grid });
+  res.status(200).json({ snapshot });
 };
