587307e29b88d90eccb272a98926f0f75946b6ec
Tyler Johnston <tyler@Tylers-MacBook-Air.local>
2023-03-24 22:57:28 -0600

    refactored schema and signup

--- src/pages/api/signup.js
+++ src/pages/api/signup.js
@@ -1,24 +1,12 @@
 import { prisma } from "../../../utils/db-client";
 import { v4 as uuidv4 } from 'uuid';
+import bcrypt from "bcrypt";
 
 export const createUser = () =>
   async (req, res) => {
     const {email, password} = req.body
     const passwordHash = await bcrypt.hash(password, 10);
-
-    const userCount = await prisma.user.count();
-    if (userCount > 1) {
-      // check if an email is already in use
-      const existingEmail = await prisma.user.findFirst({
-        where: {
-          email,
-        }
-      });
-      if (existingEmail) {
-        return res.status(400).json({message: "this email is already in use"});
-      }
-    }
-
+    
     const user = await prisma.user.create({
       data: {
         email,
@@ -28,15 +16,13 @@ export const createUser = () =>
             token: uuidv4()
           }]
         }
-      },
-      include: { 
-        sessions: true
-       }
+      }
     });
     
     res.cookie("session-token", user.sessions[0].token, {
       httpOnly: true,
       maxAge: 60000 * 10
     });
-    res.json({ user });
+    
+    res.status(200).json({ user });
   }
